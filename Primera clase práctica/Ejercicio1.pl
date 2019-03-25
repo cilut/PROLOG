703a37e0ad404b0e17032cb49a4d5813e635234d
Carlos <carlos.mateos.martin@alumnos.upm.es>
2019-03-25 12:00:43 +0100

    Añadidos ejemplos de clase

---
+++ Primera clase práctica/Ejercicio1.pl
@@ -0,0 +1,23 @@
+elimina(corleone,solozzo) :-
+	controla(corleone,manhattan),
+	controla(corleone,brooklyn).
+elimina(solozzo,corleone) :-
+	controla(solozzo,corleone),
+	apoya(roth,solozzo).
+controla(solozzo,bronx) :-
+	apoya(roth,solozzo).
+controla(solozzo,harlem) :-
+	apoya(roth,solozzo).
+controla(corleone, manhattan) :-
+	apoya(roth,corleone).
+controla(corleone, brooklyn) :-
+	apoya(roth,corleone).
+
+controla(solozzo,droga).
+controla(corleone,juego).
+apoya(roth,P) :-
+	garantiza(P,roth).
+controla(P,policia) :-
+	controla(P,juego).
+garantiza(X,Y) :-
+	controla(X,policia).
